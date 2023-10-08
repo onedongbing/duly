2737bd096dc6394c9922b0335df8485dc6935ad3
王东斌 <dongbin.wang@xtransfer.cn>
2023-10-08 19:43:44 +0800

    完善转换函数

--- packages/api-wdb/netlify/functions/notion-test.ts
+++ packages/api-wdb/netlify/functions/notion-test.ts
@@ -1,43 +1,17 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-import { Client } from "@notionhq/client";
-import { Response } from "@netlify/functions/dist/function/response";
 import { notionService } from "@/utils";
-
-const transformer = (page: any) => {
-	let data: any = {};
-
-	for (const key in page.properties) {
-		switch (page.properties[key].type) {
-			case "relation":
-				data[key] = page.properties[key].relation[0].id;
-				break;
-
-			case "title":
-			case "rich_text":
-				data[key] =
-					page.properties[key][page.properties[key].type][0]?.text?.content;
-				break;
-
-			default:
-				data[key] = page.properties[key];
-				break;
-		}
-	}
-
-	return data;
-}
 
 const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext
 ) => {
 
-  const databaseId = "924af3e47949421d987fdd971be681da";
+  const databaseId = "45bc8fd6a5a04a5781c1a66817fd63b7";
 	const response = await notionService.query({database_id: databaseId})
 	
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: {response} }),
+    body: JSON.stringify(response),
     headers: {
       "Access-Control-Allow-Origin": "*",
     },
--- packages/api-wdb/netlify/utils/notion/notionService.ts
+++ packages/api-wdb/netlify/utils/notion/notionService.ts
@@ -1,7 +1,7 @@
 import { Client } from "@notionhq/client"
 import { QueryDatabaseParameters } from "@notionhq/client/build/src/api-endpoints"
 
-const auth = "secret_vOLCV47LEk6jpRfc3AtX9Bueg1SjO7h5ugZNZOzcnH8"
+const auth = "secret_7GmzPqynQqtNNI9xYdrwk7qFCxiNyZb1gvTaDl8sob9"
 
 const client = new Client({ auth })
 
@@ -12,7 +12,34 @@ class NotionService {
   async query(data: QueryDatabaseParameters) {
     const response = await this.client.databases.query(data)
 
-    return response.results
+    return this.transfer(response.results)
+  }
+
+  /** 转换结果 */
+  private transfer(page: any): any {
+    if (Array.isArray(page)) return page.map(item => this.transfer(item))
+
+    const data: any = {}
+
+    for (const key in page.properties) {
+      const value = page.properties[key]
+      const type = value.type
+      switch (type) {
+        case "relation":
+          data[key] = value["relation"][0].id
+          break
+
+        case "title":
+        case "rich_text":
+          data[key] = value[type][0]?.text?.content
+          break
+
+        default:
+          data[key] = value[type]
+          break
+      }
+    }
+    return data
   }
 }
 
